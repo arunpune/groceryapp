6005e8300e20960d4adc77db400470d8d87f44a0
arunpune <arunkg99@gmail.com>
2022-08-23 21:38:40 +0530

    backend by arun

---
+++ backend/app/config/db.config.js
@@ -0,0 +1,10 @@
+/* module.exports = {
+  HOST: "localhost",
+  PORT: 27017,
+  DB: "bezkoder_db"
+}; */
+
+module.exports = {
+  ATLAS_URI:
+    "mongodb+srv://mean123:mean123@mean123.rpjtw9q.mongodb.net/test2?retryWrites=true&w=majority",
+};
